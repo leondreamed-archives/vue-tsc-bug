ce68588c5e462695dc9c85efcfdee8e8e87a2d2c
Leon Si <contact@leonzalion.com>
2022-02-02 19:03:09 -0500

    commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineEmits<{
+	(e: "a"): string;
+}>();
+
+const count = 0;
+</script>
+
+<template>
+	{{ count }}
+</template>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
